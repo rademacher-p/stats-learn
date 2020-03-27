02eba7a2be6c4b139cb660796acb14380a09e92d
Paul Rademacher <52750620+rademacher-p@users.noreply.github.com>
2020-03-27 17:10:44 -0400

    Commit.

--- Code/Py/main.py
+++ Code/Py/main.py
@@ -11,11 +11,13 @@ from scipy import stats
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+from tensorflow_probability import distributions as tfd
+
 from simplex_grid import simplex_grid
 
 rng = default_rng()
 
-# N = 10
+#%% Continuous sets
 
 theta_m = stats.beta(a=.9, b=.9)
 
@@ -45,10 +47,11 @@ plt.suptitle(f'Model, (X,Y) = ({X:.2f},{Y:.2f})')
 
 
 
-#%%
+#%% Discrete sets
 
 Y_set = np.array(['a', 'b', 'c'])
-X_set = np.arange(2)
+# Y_set = np.array(['a'])
+X_set = np.arange(1)
 
 YX_set = np.array([(y, x) for y in Y_set for x in X_set],
                   dtype=[('y', Y_set.dtype), ('x', X_set.dtype)]).reshape(Y_set.shape + X_set.shape)
@@ -57,31 +60,45 @@ YX_set = np.array([(y, x) for y in Y_set for x in X_set],
 # alpha = 5*np.ones(Y_set.shape + X_set.shape)
 alpha = rng.uniform(1, 10, Y_set.shape + X_set.shape)
 
-prior = stats.dirichlet(alpha.flatten())
+t_plt = simplex_grid(10, alpha.size).reshape((-1,) + alpha.shape)
 
 
-t = simplex_grid(10, alpha.size)
-plot_dir = prior.pdf(t.T)
+# prior = stats.dirichlet(alpha.flatten())
+# prior_plt = prior.pdf(t_plt.reshape((-1, alpha.size)).T)
+# theta_pmf = prior.rvs().reshape(alpha.shape)
 
-_, ax = plt.subplots(num='prior', clear=True, subplot_kw={'projection': '3d'})
-sc = ax.scatter(t[:, 0], t[:, 1], t[:, 2], s=15, c=plot_dir)
-ax.view_init(35, 45)  # TODO: check
-plt.colorbar(sc)
+prior = tfd.Dirichlet(alpha.flatten())
+# prior = tfd.Deterministic(rng.choice(t_plt).flatten())    # TODO: error, need multivariate
+prior_plt = prior.prob(t_plt.reshape((-1, alpha.size)))
+theta_pmf = prior.sample().numpy().reshape(alpha.shape)
+
+
+
+if alpha.shape == (3, 1):
+    _, ax = plt.subplots(num='prior', clear=True, subplot_kw={'projection': '3d'})
+    sc = ax.scatter(t_plt[:, 0], t_plt[:, 1], t_plt[:, 2], s=15, c=prior_plt)
+    ax.view_init(35, 45)
+    plt.colorbar(sc)
 
 
 # def prior():
-#     theta = drc(alpha.flatten()).rvs().reshape(alpha.shape)
+#     theta = stats.dirichlet(alpha.flatten()).rvs().reshape(alpha.shape) # TODO: persistence?
 #     return stats.rv_discrete(name='theta', values=(vals, theta.flatten()))
 
-theta_pmf = prior.rvs().reshape(alpha.shape)
 
 q = rng.choice(YX_set.flatten(), p=theta_pmf.flatten())
 
+z = stats.rv_discrete(name='z', values=(['a','b','c'], [.2,.5,.3]))     # cant handle non-integral values...
+z = tfd.Categorical(theta_pmf.flatten())    # only returns integers...
+
+
+
 # vals = YX_set
-vals = np.arange(1, 4).reshape(3, 1)
+vals = np.arange(alpha.size).reshape(alpha.shape)
 # vals = list('asdfer')
 theta = stats.rv_discrete(name='theta', values=(vals, theta_pmf))
 
 plt.figure(num='theta_pmf', clear=True)
 plt.stem(vals.flatten(), theta.pmf(vals).flatten(), use_line_collection=True)
+
 
