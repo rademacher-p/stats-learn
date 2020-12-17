4dd27adad58f056aa92823e71346544047c5591d
Paul Rademacher <paul.g.rademacher@gmail.com>
2020-12-17 16:02:33 -0500

    MATLAB result replication automation

--- Code/PGR_thesis/validate_mat.py
+++ Code/PGR_thesis/validate_mat.py
@@ -3,6 +3,8 @@ Main.
 """
 
 from copy import deepcopy
+import datetime
+from typing import Iterable
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -16,56 +18,116 @@ from thesis.predictors import (ModelRegressor, BayesRegressor, ModelClassifier, 
 # plt.style.use('seaborn')
 
 
-# %% Sim
+def dir_figs(n_train, alpha_0, p_y=(.5, .5), n_mc=1000, do_bayes=False, alpha_0_true=3):
 
-model_x = rand_elements.FiniteRV([0, .5], p=None)
+    model_x = rand_elements.FiniteRV([0, .5], p=None)
 
-model = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in (.5, .5)], model_x)
+    model = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in p_y], model_x)
 
-model = bayes_models.Dirichlet(model, alpha_0=3)
+    if do_bayes:
+        model = bayes_models.Dirichlet(model, alpha_0_true)
+
+    if isinstance(alpha_0, Iterable):
+        alpha_0_init = alpha_0[0]
+        dir_params = {'alpha_0': alpha_0}.copy()
+        if isinstance(n_train, Iterable):
+            if len(n_train) >= len(alpha_0):
+                plot_type = 'n'
+        else:
+            plot_type = 'a'
+    else:
+        alpha_0_init = alpha_0
+        dir_params = None
+        plot_type = 'n'
+        if not isinstance(n_train, Iterable):
+            return
+
+    prior_mean = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in (.5, .5)], model_x)
+    dir_predictor = BayesRegressor(bayes_models.Dirichlet(prior_mean, alpha_0_init), name='Dir')
+
+    if plot_type == 'a':
+        dir_predictor.plot_loss_eval(model, params=dir_params, n_train=n_train, n_test=1, n_mc=n_mc, verbose=True,
+                                     rng=None)
+    elif plot_type == 'n':
+        predictors = [
+            ModelRegressor(model, name=r'$f_{opt}$'),
+            dir_predictor,
+        ]
+
+        params = [None, dir_params]
+
+        plot_loss_eval_compare(predictors, model, params, n_train=n_train, n_test=1, n_mc=n_mc,
+                               verbose=True, ax=None, rng=None)
+
+    plt.savefig(f'images/{datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S")}')
 
 
-prior_mean = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in (.5, .5)], model_x)
-dir_predictor = BayesRegressor(bayes_models.Dirichlet(prior_mean, alpha_0=3), name='Dir')
+def main():
+    args = [
+        (np.arange(0, 50, 1), [2, 16], (.5, .5)),
+        (np.arange(0, 50, 1), [2, 16], (.9, .9)),
+        ([0, 2, 8], .001 + np.arange(0, 10, .1), (.5, .5)),
+        ([0, 2, 8], .001 + np.arange(0, 20, .1), (.9, .9)),
+    ]
+
+    for args_i in args:
+        dir_figs(*args_i, n_mc=20000)
 
 
-# Plotting
-
-n_train = 2
-# n_train = [2, 4]
-# n_train = np.arange(0, 11, 1)
-
-# dir_params = None
-dir_params = {'alpha_0': .001 + np.arange(2, 4, .2)}
+if __name__ == '__main__':
+    main()
 
 
+# # %% Sim
 #
-# loss = dir_predictor.loss_eval(model, params=dir_params, n_train=n_train, n_test=1, n_mc=20000, verbose=True, rng=None)
-# print(loss)
-
-dir_predictor.plot_loss_eval(model, params=dir_params, n_train=n_train, n_test=1, n_mc=10000, verbose=True, rng=None)
-
-bayes_risk = 0.
-for x in model.space['x'].values:
-    alpha_m = model.prior_mean.model_x.pf(x)
-    weight = (alpha_m + 1 / (model.alpha_0 + n_train)) / (alpha_m + 1 / model.alpha_0)
-    bayes_risk += alpha_m * model.prior_mean.model_y_x(x).cov * weight
-
-print(bayes_risk)
-
-
+# model_x = rand_elements.FiniteRV([0, .5], p=None)
 #
-predictors = [
-    ModelRegressor(model, name=r'$f_{opt}$'),
-    dir_predictor,
-]
-
-params = [None, dir_params]
-
-# plot_loss_eval_compare(predictors, model, params, n_train=n_train, n_test=1, n_mc=4000,
-#                        verbose=True, ax=None, rng=None)
-# plot_predict_stats_compare(predictors, model, params, x=None, n_train=n_train, n_mc=300, do_std=True,
-#                            verbose=True, ax=None, rng=None)
-
-
-qq = 1
+# model = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in (.5, .5)], model_x)
+#
+# model = bayes_models.Dirichlet(model, alpha_0=3)
+#
+#
+# prior_mean = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in (.5, .5)], model_x)
+# dir_predictor = BayesRegressor(bayes_models.Dirichlet(prior_mean, alpha_0=3), name='Dir')
+#
+#
+# # Plotting
+#
+# n_train = 2
+# # n_train = [2, 4]
+# # n_train = np.arange(0, 11, 1)
+#
+# # dir_params = None
+# dir_params = {'alpha_0': .001 + np.arange(2, 4, .2)}
+#
+#
+# #
+# # loss = dir_predictor.loss_eval(model, params=dir_params, n_train=n_train, n_test=1, n_mc=20000, verbose=True, rng=None)
+# # print(loss)
+#
+# dir_predictor.plot_loss_eval(model, params=dir_params, n_train=n_train, n_test=1, n_mc=10000, verbose=True, rng=None)
+#
+# bayes_risk = 0.
+# for x in model.space['x'].values:
+#     alpha_m = model.prior_mean.model_x.pf(x)
+#     weight = (alpha_m + 1 / (model.alpha_0 + n_train)) / (alpha_m + 1 / model.alpha_0)
+#     bayes_risk += alpha_m * model.prior_mean.model_y_x(x).cov * weight
+#
+# print(bayes_risk)
+#
+#
+# #
+# predictors = [
+#     ModelRegressor(model, name=r'$f_{opt}$'),
+#     dir_predictor,
+# ]
+#
+# params = [None, dir_params]
+#
+# # plot_loss_eval_compare(predictors, model, params, n_train=n_train, n_test=1, n_mc=4000,
+# #                        verbose=True, ax=None, rng=None)
+# # plot_predict_stats_compare(predictors, model, params, x=None, n_train=n_train, n_mc=300, do_std=True,
+# #                            verbose=True, ax=None, rng=None)
+#
+#
+# qq = 1
