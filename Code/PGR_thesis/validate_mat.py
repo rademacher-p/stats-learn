643ab32a0c183ea7ceef9acb37baa645c05d379a
Paul Rademacher <paul.g.rademacher@gmail.com>
2020-12-17 12:04:27 -0500

    Bayes risk results WIP

--- Code/PGR_thesis/validate_mat.py
+++ Code/PGR_thesis/validate_mat.py
@@ -22,29 +22,34 @@ supp_x = np.array([0, .5])
 model_x = rand_elements.FiniteRV(supp_x, p=None)
 
 model = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in (.5, .5)], model_x)
-model = bayes_models.Dirichlet(model, alpha_0=1)
+model = bayes_models.Dirichlet(model, alpha_0=3)
 
 # prior_mean = deepcopy(model.prior_mean)
 # prior_mean = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in (.5, .5)], model_x)
 
 bayes_predictor = BayesRegressor(model, name='Dir')
+# bayes_predictor = BayesRegressor(bayes_models.Dirichlet(model, alpha_0=1), name='Dir')
 # bayes_predictor = BayesRegressor(bayes_models.Dirichlet(prior_mean, alpha_0=1), name='Dir')
 # bayes_predictor = BayesClassifier(bayes_models.Dirichlet(prior_mean, alpha_0=40), name='Dir')
 
 # Plotting
 
-n_train = 2
+n_train = 5
 
-loss = bayes_predictor.loss_eval(model, params=None, n_train=n_train, n_test=1, n_mc=50000, verbose=True, rng=None)
-print(loss)
+# loss = bayes_predictor.loss_eval(model, params=None, n_train=n_train, n_test=1, n_mc=20000, verbose=True, rng=None)
+# print(loss)
+#
+# bayes_risk = 0.
+# for x in model.space['x'].values:
+#     alpha_m = model.prior_mean.model_x.pf(x)
+#     weight = (alpha_m + 1 / (model.alpha_0 + n_train)) / (alpha_m + 1 / model.alpha_0)
+#     bayes_risk += alpha_m * model.prior_mean.model_y_x(x).cov * weight
+#
+# print(bayes_risk)
 
-bayes_risk = 0.
-for x in model.space['x'].values:
-    alpha_m = model.prior_mean.model_x.pf(x)
-    weight = (alpha_m + 1 / (model.alpha_0 + n_train)) / (alpha_m + 1 / model.alpha_0)
-    bayes_risk += alpha_m * model.prior_mean.model_y_x(x).cov * weight
-
-print(bayes_risk)
+params = {'alpha_0': .01 + np.arange(0, 5, .1)}
+loss = bayes_predictor.plot_loss_eval(model, params=params, n_train=n_train, n_test=1, n_mc=10000,
+                                      verbose=True, rng=None)
 
 
 params = [
