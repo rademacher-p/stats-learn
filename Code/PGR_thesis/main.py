8262225d7fd38c128e4326e23f7565321181008e
Paul Rademacher <paul.g.rademacher@gmail.com>
2020-12-18 13:10:17 -0500

    Result module consolidation

--- Code/PGR_thesis/main.py
+++ Code/PGR_thesis/main.py
@@ -14,7 +14,7 @@ from thesis.predictors import (ModelRegressor, BayesRegressor, ModelClassifier, 
 # plt.style.use('seaborn')
 
 
-# %% Sim
+#%% Sim
 
 def poly(x, weights):
     return sum(w * x ** i for i, w in enumerate(weights))
@@ -32,6 +32,8 @@ supp_x = np.linspace(0, 1, 11, endpoint=True)
 
 model_x = rand_elements.FiniteRV(supp_x, p=None)
 
+# model = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in (.5, .5)], model_x)
+
 mean_y_x = poly(supp_x, [.5, 0, 0])
 # mean_y_x = poly(supp_x, [.3, 0, .4])
 # mean_y_x = 0.5 + 0.5 * np.sin(2*np.pi * supp_x)
@@ -46,6 +48,8 @@ model = rand_models.DataConditional(list(map(mean_to_rv, mean_y_x)), model_x)
 
 model = bayes_models.Dirichlet(model, alpha_0=10)
 
+
+# Optimal learner
 if isinstance(model, rand_models.Base):
     opt_predictor = ModelRegressor(model, name=r'$f_{\Theta}$')
 elif isinstance(model, bayes_models.Base):
@@ -56,6 +60,8 @@ else:
 
 # Dirichlet learner
 
+# prior_mean = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in (.5, .5)], model_x)
+
 w_prior = [.5, 0, 0]
 # w_prior = [.5, 0, .5]
 mean_y_x_dir = poly(supp_x, w_prior)
@@ -63,45 +69,54 @@ prior_mean = rand_models.DataConditional(list(map(mean_to_rv, mean_y_x_dir)), mo
 
 dir_predictor = BayesRegressor(bayes_models.Dirichlet(prior_mean, alpha_0=1), name='Dir')
 
+# dir_params = None
+# dir_params = {'alpha_0': [2, 16]}
+dir_params = {'alpha_0': .001 + np.arange(0, 50, 2)}
+
+
+# Normal learner
+norm_predictor = BayesRegressor(bayes_models.NormalLinear(prior_mean=w_prior, prior_cov=10 * np.eye(len(w_prior)),
+                                                          basis_y_x=None, cov_y_x=.1,
+                                                          model_x=model_x), name='Norm')
+
+norm_params = None
+# norm_params = {'prior_cov': [10, 0.05]}
+# norm_params = {'prior_cov': [10]}
+
 
 # Plotting
 
 # n_train = 10
-# n_train = [0, 5, 10]
-n_train = np.arange(0, 100, 5)
+n_train = [0, 5, 10]
+# n_train = np.arange(0, 50, 5)
 
-# dir_params = None
-dir_params = {'alpha_0': [1, 10, 100]}
-# dir_params = {'alpha_0': .001 + np.arange(0, 50, 2)}
-
-
-#
 # print(dir_predictor.loss_eval(model, dir_params, n_train, n_test=1, n_mc=20000, verbose=True, rng=None))
-dir_predictor.plot_loss_eval(model, dir_params, n_train, n_test=1, n_mc=5000, verbose=True, rng=None)
+# dir_predictor.plot_loss_eval(model, dir_params, n_train, n_test=1, n_mc=5000, verbose=True, rng=None)
 
 
-if isinstance(opt_predictor, BayesRegressor):
-    print(opt_predictor.bayes_risk_min(n_train))
-
-#
-predictors = [
-    opt_predictor,
-    # BayesRegressor(bayes_models.NormalLinear(prior_mean=w_prior, prior_cov=10 * np.eye(len(w_prior)),
-    #                                          basis_y_x=None, cov_y_x=.1,
-    #                                          model_x=model_x), name='Norm'),
-    dir_predictor,
+temp = [
+    # (opt_predictor, None),
+    # (norm_predictor, norm_params),
+    (dir_predictor, dir_params),
 ]
 
-params = [
-    None,
-    # None,
-    # {'prior_cov': [10, 0.05]},
-    # {'prior_cov': [10]},
-    dir_params,
-]
+predictors, params = list(zip(*temp))
 
 
-# plot_loss_eval_compare(predictors, model, params, n_train=n_train, n_test=1, n_mc=100,
-#                        verbose=True, ax=None, rng=None)
+plot_loss_eval_compare(predictors, model, params, n_train=n_train, n_test=1, n_mc=2000,
+                       verbose=True, ax=None, rng=None)
 # plot_predict_stats_compare(predictors, model, params, x=None, n_train=n_train, n_mc=300, do_std=True,
 #                            verbose=True, ax=None, rng=None)
+
+
+print('')
+if isinstance(model, rand_models.Base):
+    risk_an = opt_predictor.risk_min()
+    print(f"Min risk = {risk_an}")
+elif isinstance(model, bayes_models.Base):
+    risk_an = opt_predictor.bayes_risk_min(n_train)
+    print(f"Min Bayes risk = {risk_an}")
+else:
+    raise TypeError
+
+print('Done')
--- Code/PGR_thesis/thesis/_deprecated/validate_mat.py
+++ Code/PGR_thesis/thesis/_deprecated/validate_mat.py
@@ -12,6 +12,7 @@ from matplotlib import pyplot as plt
 from thesis.random import elements as rand_elements, models as rand_models
 from thesis.bayes import models as bayes_models
 from thesis.predictors import (ModelRegressor, BayesRegressor, ModelClassifier, BayesClassifier,
+                               Model as ModelPredictor, Bayes as BayesPredictor,
                                plot_loss_eval_compare, plot_predict_stats_compare,
                                loss_eval_compare, predict_stats_compare)
 
@@ -87,8 +88,10 @@ model_x = rand_elements.FiniteRV([0, .5], p=None)
 
 model = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in (.5, .5)], model_x)
 
-model = bayes_models.Dirichlet(model, alpha_0=3)
+# model = bayes_models.Dirichlet(model, alpha_0=3)
 
+
+# Optimal learner
 if isinstance(model, rand_models.Base):
     opt_predictor = ModelRegressor(model, name=r'$f_{\Theta}$')
 elif isinstance(model, bayes_models.Base):
@@ -101,26 +104,21 @@ else:
 prior_mean = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in (.5, .5)], model_x)
 dir_predictor = BayesRegressor(bayes_models.Dirichlet(prior_mean, alpha_0=3), name='Dir')
 
+# dir_params = None
+dir_params = {'alpha_0': [2, 16]}
+# dir_params = {'alpha_0': .001 + np.arange(2, 4, .2)}
+
+# print(dir_predictor.loss_eval(model, dir_params, n_train, n_test=1, n_mc=20000, verbose=True, rng=None))
+# dir_predictor.plot_loss_eval(model, dir_params, n_train, n_test=1, n_mc=1000, verbose=True, rng=None)
+
 
 # Plotting
 
 # n_train = 2
-n_train = [2, 4]
-# n_train = np.arange(0, 11, 1)
-
-# dir_params = None
-dir_params = {'alpha_0': .001 + np.arange(2, 4, .2)}
+# n_train = [2, 4]
+n_train = np.arange(0, 11, 1)
 
 
-#
-# print(dir_predictor.loss_eval(model, dir_params, n_train, n_test=1, n_mc=20000, verbose=True, rng=None))
-dir_predictor.plot_loss_eval(model, dir_params, n_train, n_test=1, n_mc=1000, verbose=True, rng=None)
-
-if isinstance(opt_predictor, BayesRegressor):
-    print(opt_predictor.bayes_risk_min(n_train))
-
-
-#
 predictors = [
     opt_predictor,
     dir_predictor,
@@ -128,7 +126,20 @@ predictors = [
 
 params = [None, dir_params]
 
-# plot_loss_eval_compare(predictors, model, params, n_train=n_train, n_test=1, n_mc=4000,
-#                        verbose=True, ax=None, rng=None)
+
+plot_loss_eval_compare(predictors, model, params, n_train=n_train, n_test=1, n_mc=4000,
+                       verbose=True, ax=None, rng=None)
 # plot_predict_stats_compare(predictors, model, params, x=None, n_train=n_train, n_mc=300, do_std=True,
 #                            verbose=True, ax=None, rng=None)
+
+
+if isinstance(model, rand_models.Base):
+    risk_an = opt_predictor.risk_min()
+    print(f"Min risk = {risk_an}")
+elif isinstance(model, bayes_models.Base):
+    risk_an = opt_predictor.bayes_risk_min(n_train)
+    print(f"Min Bayes risk = {risk_an}")
+else:
+    raise TypeError
+
+print('Done')
