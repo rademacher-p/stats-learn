110b541ecf70be0be1f08d85525708578c6288ed
Paul Rademacher <paul.g.rademacher@gmail.com>
2020-12-21 15:13:55 -0500

    WIP

--- Code/PGR_thesis/main.py
+++ Code/PGR_thesis/main.py
@@ -37,7 +37,7 @@ model_x = rand_elements.FiniteRV(supp_x, p=None)
 # model = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in (.5, .5)], model_x)
 
 # mean_y_x = poly(supp_x, [.5, 0, 0])
-mean_y_x = poly(supp_x, [.3, 0, .4])
+mean_y_x = poly(supp_x, [0, 0, 1])
 # mean_y_x = 0.5 + 0.5 * np.sin(2*np.pi * supp_x)
 # mean_y_x = 1 / (1 + np.exp(10 * supp_x))
 # mean_y_x = 1 / (2 + np.sin(2*np.pi * supp_x))
@@ -65,7 +65,7 @@ else:
 
 # prior_mean = rand_models.DataConditional([rand_elements.Finite([0, .5], [p, 1 - p]) for p in (.5, .5)], model_x)
 
-w_prior = [.5, 0, 0]
+w_prior = [.5, 0]
 # w_prior = [.5, 0, .5]
 mean_y_x_dir = poly(supp_x, w_prior)
 prior_mean = rand_models.DataConditional(list(map(mean_to_rv, mean_y_x_dir)), model_x)
@@ -74,7 +74,8 @@ dir_predictor = BayesRegressor(bayes_models.Dirichlet(prior_mean, alpha_0=1), na
 
 # dir_params = None
 # dir_params = {'alpha_0': [2, 4, 8, 16]}
-dir_params = {'alpha_0': .001 + np.arange(0, 80, 5)}
+dir_params = {'alpha_0': [.1, 50]}
+# dir_params = {'alpha_0': .001 + np.arange(0, 80, 5)}
 
 
 # Normal learner
@@ -82,23 +83,23 @@ norm_predictor = BayesRegressor(bayes_models.NormalLinear(prior_mean=w_prior, pr
                                                           basis_y_x=None, cov_y_x=.1,
                                                           model_x=model_x), name='Norm')
 
-norm_params = None
-# norm_params = {'prior_cov': [10, 0.05]}
+# norm_params = None
+norm_params = {'prior_cov': [10, 0.05]}
 # norm_params = {'prior_cov': [10]}
 
 
 # Plotting
 
 # n_train = 10
-n_train = [0, 10, 20]
-# n_train = np.arange(0, 50, 1)
+# n_train = [0, 10, 20]
+n_train = np.arange(0, 200, 10)
 
 # print(dir_predictor.risk_eval_sim(model, dir_params, n_train, n_test=1, n_mc=20000, verbose=True, rng=None))
 # dir_predictor.plot_risk_eval_sim(model, dir_params, n_train, n_test=1, n_mc=5000, verbose=True, rng=None)
 
 
 temp = [
-    # (opt_predictor, None),
+    (opt_predictor, None),
     # (norm_predictor, norm_params),
     (dir_predictor, dir_params),
 ]
@@ -106,9 +107,9 @@ temp = [
 predictors, params = list(zip(*temp))
 
 
-plot_risk_eval_sim_compare(predictors, model, params, n_train=n_train, n_test=1, n_mc=200,
-                           verbose=True, ax=None, rng=None)
-# plot_risk_eval_comp_compare(predictors, model, params, n_train, n_test=1, verbose=False, ax=None)
+# plot_risk_eval_sim_compare(predictors, model, params, n_train=n_train, n_test=1, n_mc=200,
+#                            verbose=True, ax=None, rng=None)
+plot_risk_eval_comp_compare(predictors, model, params, n_train, n_test=1, verbose=False, ax=None)
 
 # plot_predict_stats_compare(predictors, model, params, x=None, n_train=n_train, n_mc=300, do_std=True,
 #                            verbose=True, ax=None, rng=None)
