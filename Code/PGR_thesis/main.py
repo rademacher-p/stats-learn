ab0017bceee2651a33ac6b88b9e8ff54f3982b2a
Paul Rademacher <paul.g.rademacher@gmail.com>
2021-02-04 12:21:30 -0500

    Added package matplotlibrc file
    Minor edits to deprecate grid control in favor of RC param user control

--- Code/PGR_thesis/main.py
+++ Code/PGR_thesis/main.py
@@ -112,15 +112,15 @@ norm_predictor = BayesRegressor(bayes_models.NormalLinear(prior_mean=w_prior, pr
 
 # norm_params = None
 # norm_params = {'prior_cov': [10, 0.05]}
-# norm_params = {'prior_cov': [100, .01]}
-norm_params = {'prior_cov': [100]}
+norm_params = {'prior_cov': [100, .01]}
+# norm_params = {'prior_cov': [100]}
 
 
 # Plotting
 
-n_train = 200
+# n_train = 200
 # n_train = [0, 10, 100]
-# n_train = np.arange(0, 605, 5)
+n_train = np.arange(0, 650, 50)
 
 # print(dir_predictor.risk_eval_sim(model, dir_params, n_train, n_test=1, n_mc=20000, verbose=True, rng=None))
 # dir_predictor.plot_risk_eval_sim(model, dir_params, n_train, n_test=1, n_mc=5000, verbose=True, rng=None)
@@ -142,12 +142,12 @@ plt.rc('text.latex', preamble=r"\usepackage{amsmath}")
 
 predictors, params = list(zip(*temp))
 
-# plot_risk_eval_sim_compare(predictors, model_eval, params, n_train=n_train, n_test=1, n_mc=50,
-#                            verbose=True, ax=None, rng=None)
+plot_risk_eval_sim_compare(predictors, model_eval, params, n_train=n_train, n_test=1, n_mc=50,
+                           verbose=True, ax=None, rng=None)
 # plot_risk_eval_comp_compare(predictors, model_eval, params, n_train, n_test=1, verbose=False, ax=None)
 
-plot_predict_stats_compare(predictors, model_eval, params, x=None, n_train=n_train, n_mc=50,
-                           do_std=True, verbose=True, ax=None, rng=None)
+# plot_predict_stats_compare(predictors, model_eval, params, x=None, n_train=n_train, n_mc=50,
+#                            do_std=True, verbose=True, ax=None, rng=None)
 
 
 # print(f"\nAnalytical Risk = {opt_predictor.evaluate_comp(n_train=n_train)}")
