475cecfa91539166a786afa03055379e1bf71152
Paul Rademacher <paul.g.rademacher@gmail.com>
2021-03-18 09:43:11 -0400

    WIP

--- Code/PGR_thesis/main.py
+++ Code/PGR_thesis/main.py
@@ -128,12 +128,12 @@ dir_predictor = BayesRegressor(bayes_models.Dirichlet(prior_mean, alpha_0=10),
 
 # dir_params = None
 # dir_params = {'alpha_0': [10, 1000]}
-dir_params = {'alpha_0': [10]}
+# dir_params = {'alpha_0': [10]}
 # dir_params = {'alpha_0': [.01, 100]}
 # dir_params = {'alpha_0': [40, 400, 4000]}
 # dir_params = {'alpha_0': 1e-6 + np.linspace(0, 20, 100)}
 # dir_params = {'alpha_0': np.logspace(-1., 5., 60)}
-# dir_params = {'alpha_0': np.logspace(-2., 4., 40)}
+dir_params = {'alpha_0': np.logspace(-3., 3., 100)}
 
 if do_bayes:  # add true bayes model concentration
     if model_eval.alpha_0 not in dir_params['alpha_0']:
@@ -141,14 +141,14 @@ if do_bayes:  # add true bayes model concentration
 
 
 ###
-n_t_iter = [4, 128, 4096]
-# n_t_iter = [2, 4, 8]
-# n_t_iter = [2, 4, 8, 16]
+# n_t_iter = [4, 128, 4096]
+# n_t_iter = [4, 16, 32, 64, 128]
+n_t_iter = [2, 4, 8, 16]
 # n_t_iter = 2 ** np.arange(1, 6)
 # n_t_iter = list(range(1, 33, 1))
 
-scale_alpha = False
-# scale_alpha = True
+# scale_alpha = False
+scale_alpha = True
 
 dir_predictors = []
 dir_params_full = [deepcopy(dir_params) for __ in n_t_iter]
@@ -179,7 +179,7 @@ norm_params = {'prior_cov': [.1, .001]}
 
 # Plotting
 
-n_train = 400
+n_train = 4
 # n_train = [0, 10, 50, 100]
 # n_train = [0, 800, 3000]
 # n_train = [0, 100, 200, 400, 800]
@@ -194,10 +194,10 @@ n_train = 400
 
 
 temp = [
-    (opt_predictor, None),
+    # (opt_predictor, None),
     # (dir_predictor, dir_params),
     *(zip(dir_predictors, dir_params_full)),
-    (norm_predictor, norm_params),
+    # (norm_predictor, norm_params),
 ]
 
 # TODO: discrete plot for predict stats
@@ -208,11 +208,11 @@ plt.rc('text.latex', preamble=r"\usepackage{amsmath} \usepackage{upgreek} \usepa
 
 predictors, params = list(zip(*temp))
 
-# plot_risk_eval_sim_compare(predictors, model_eval, params, n_train, n_mc=50000, verbose=True, ax=None, rng=None)
+plot_risk_eval_sim_compare(predictors, model_eval, params, n_train, n_mc=500, verbose=True, ax=None, rng=None)
 # plot_risk_eval_comp_compare(predictors, model_eval, params, n_train, verbose=False, ax=None)
 
-plot_predict_stats_compare(predictors, model_eval, params, x=None, n_train=n_train, n_mc=50000,
-                           do_std=True, verbose=True, ax=None, rng=None)
+# plot_predict_stats_compare(predictors, model_eval, params, x=None, n_train=n_train, n_mc=50000,
+#                            do_std=True, verbose=True, ax=None, rng=None)
 
 
 # plot_risk_disc(predictors, model_eval, params, n_train, n_test=1, n_mc=50000, verbose=True, ax=None, rng=None)
