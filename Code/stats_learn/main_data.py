7dece40b7a89b6725403fe4c9f5dcb7f59c46393
Paul Rademacher <paul.g.rademacher@gmail.com>
2021-06-20 12:45:51 -0400

    `util.results` function move refactor

--- Code/stats_learn/main_data.py
+++ Code/stats_learn/main_data.py
@@ -6,8 +6,9 @@ from matplotlib import pyplot as plt
 import pandas as pd
 
 from stats_learn.bayes import models as bayes_models
-from stats_learn.predictors import (ModelRegressor, BayesRegressor, plot_risk_eval_sim_compare, plot_predict_stats_compare,
-                                    risk_eval_sim_compare, plot_risk_eval_comp_compare, plot_risk_disc)
+from stats_learn.predictors import (ModelRegressor, BayesRegressor)
+from stats_learn.util.results import plot_predict_stats_compare, risk_eval_sim_compare, plot_risk_eval_sim_compare, \
+    plot_risk_eval_comp_compare, plot_risk_disc
 from stats_learn.random import elements as rand_elements, models as rand_models
 from stats_learn.preprocessing import discretizer, prob_disc
 from stats_learn.util.base import vectorize_func, NOW_STR
