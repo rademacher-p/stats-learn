6d4a8cd6ab10172883cf0e389f976576f16dcb6f
Paul Rademacher <paul.g.rademacher@gmail.com>
2021-08-28 11:23:52 -0400

    Clipping needed for NN, not norm!

--- examples/discrete/regression/_clip_norm.py
+++ examples/discrete/regression/_clip_norm.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 from stats_learn.random import elements as rand_elements, models as rand_models
 from stats_learn.bayes import models as bayes_models
 from stats_learn.predictors.base import ModelRegressor, BayesRegressor
-# from stats_learn.util.data_processing import make_clipper
+from stats_learn.util.data_processing import make_clipper
 from stats_learn.util import funcs
 from stats_learn import results
 
@@ -77,32 +77,32 @@ dir_params = {'alpha_0': [10, 1000]}
 
 
 # Normal-prior LR
-norm_model = bayes_models.NormalLinear(prior_mean=w_prior, prior_cov=.1, cov_y_x=.1, model_x=model_x,
-                                       allow_singular=True)
-norm_predictor = BayesRegressor(norm_model, space=model.space, name=r'$\mathcal{N}$')
+# proc_funcs = []
+# proc_funcs = {'pre': [], 'post': [make_clipper([0, 1])]}
+proc_funcss = [[], {'pre': [], 'post': [make_clipper([0, 1])]}]
 
-norm_params = {'prior_cov': [.1, .001]}
+norm_predictors = []
+
+for proc_funcs in proc_funcss:
+    norm_model = bayes_models.NormalLinear(prior_mean=w_prior, prior_cov=.1, cov_y_x=.1, model_x=model_x,
+                                           allow_singular=True)
+    norm_predictor = BayesRegressor(norm_model, space=model.space, proc_funcs=proc_funcs,
+                                    name=r'$\mathcal{N}$'+str(len(proc_funcs)))
+    norm_predictors.append(norm_predictor)
 
 
 #
 temp = [
     (opt_predictor, None),
     (dir_predictor, dir_params),
-    (norm_predictor, norm_params),
+    *((norm_predictor, None) for norm_predictor in norm_predictors),
 ]
 predictors, params = zip(*temp)
 
 
 #%% Results
 n_test = 1000
-n_mc = 5
-
-# Sample regressor realizations
-n_train = 30
-
-d = model.rvs(n_train + n_test, rng=seed)
-d_train, d_test = np.split(d, [n_train])
-loss_full = results.plot_fit_compare(predictors, d_train, d_test, params, img_dir=img_dir, file=file)
+n_mc = 50
 
 # Prediction mean/variance, comparative
 n_train = 400
@@ -110,20 +110,9 @@ y_stats_full, loss_full = results.assess_compare(predictors, model, params, n_tr
                                                  stats=('mean', 'std'), verbose=True, plot_stats=True, print_loss=True,
                                                  img_dir=img_dir, file=file, rng=seed)
 
-# Dirichlet-based prediction mean/variance, varying N
-n_train = [0, 800, 4000]
-y_stats_full, loss_full = dir_predictor.assess(model, {'alpha_0': [1000]}, n_train, n_test, n_mc, stats=('mean', 'std'),
-                                               verbose=True, plot_stats=True, print_loss=True,
-                                               img_dir=img_dir, file=file, rng=seed)
-
 # Squared-Error vs. training data volume N
 n_train = np.arange(0, 4050, 50)
 y_stats_full, loss_full = results.assess_compare(predictors, model, params, n_train, n_test, n_mc, verbose=True,
                                                  plot_loss=True, print_loss=True,
                                                  img_dir=img_dir, file=file, rng=seed)
 
-# Squared-Error vs. prior localization alpha_0
-n_train = [0, 100, 200, 400, 800]
-y_stats_full, loss_full = dir_predictor.assess(model, {'alpha_0': np.logspace(0., 5., 100)}, n_train, n_test, n_mc,
-                                               verbose=True, plot_loss=True, print_loss=True,
-                                               img_dir=img_dir, file=file, rng=seed)
