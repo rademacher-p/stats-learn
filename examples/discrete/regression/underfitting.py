cff2a37ba080cdf9afe36b8387cff03ec4bd4bc0
Paul Rademacher <paul.rademacher@nrl.navy.mil>
2021-08-31 14:48:45 -0400

    MLP build update

--- examples/discrete/regression/underfitting.py
+++ examples/discrete/regression/underfitting.py
@@ -105,14 +105,14 @@ d = model.rvs(n_train + n_test, rng=seed)
 d_train, d_test = np.split(d, [n_train])
 loss_full = results.plot_fit_compare(predictors, d_train, d_test, params, log_path=log_path, img_path=img_path)
 
-# Prediction mean/variance, comparative
-n_train = 128
-y_stats_full, loss_full = results.assess_compare(predictors, model, params, n_train, n_test, n_mc,
-                                                 stats=('mean', 'std'), verbose=True, plot_stats=True, print_loss=True,
-                                                 log_path=log_path, img_path=img_path, rng=seed)
-
-# Squared-Error vs. training data volume N
-n_train = np.insert(2**np.arange(11), 0, 0)
-y_stats_full, loss_full = results.assess_compare(predictors, model, params, n_train, n_test, n_mc, verbose=True,
-                                                 plot_loss=True, print_loss=True, log_path=log_path, img_path=img_path,
-                                                 rng=seed)
+# # Prediction mean/variance, comparative
+# n_train = 128
+# y_stats_full, loss_full = results.assess_compare(predictors, model, params, n_train, n_test, n_mc,
+#                                                  stats=('mean', 'std'), verbose=True, plot_stats=True, print_loss=True,
+#                                                  log_path=log_path, img_path=img_path, rng=seed)
+#
+# # Squared-Error vs. training data volume N
+# n_train = np.insert(2**np.arange(11), 0, 0)
+# y_stats_full, loss_full = results.assess_compare(predictors, model, params, n_train, n_test, n_mc, verbose=True,
+#                                                  plot_loss=True, print_loss=True, log_path=log_path, img_path=img_path,
+#                                                  rng=seed)
