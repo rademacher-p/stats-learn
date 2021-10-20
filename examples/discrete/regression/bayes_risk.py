ca2442e96911076655d71bfedd7b0372002e3881
Paul Rademacher <paul.rademacher@nrl.navy.mil>
2021-10-20 10:54:35 -0400

    Cleanup

--- examples/discrete/regression/bayes_risk.py
+++ examples/discrete/regression/bayes_risk.py
@@ -1,22 +1,23 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+from stats_learn.util.base import get_now
 from stats_learn.random import elements as rand_elements, models as rand_models
 from stats_learn.bayes import models as bayes_models
 from stats_learn.predictors.base import BayesRegressor
 
-# plt.style.use('../../../images/style.mplstyle')
+plt.style.use('../../../images/style.mplstyle')
 
 # seed = None
 seed = 12345
 
-log_path = None
-img_path = None
+# log_path = None
+# img_path = None
 
-# # TODO: remove path stuff and image names below before release
-# base_path = __file__[__file__.rfind('/')+1:].removesuffix('.py') + '_temp/'
-# log_path = base_path + 'log.md'
-# img_dir = base_path + f'images/{get_now()}/'
+# TODO: remove path stuff and image names below before release
+base_path = __file__[__file__.rfind('/')+1:].removesuffix('.py') + '_temp/'
+log_path = base_path + 'log.md'
+img_dir = base_path + f'images/{get_now()}/'
 
 
 # %% Model and optimal predictor
@@ -39,13 +40,13 @@ dir_predictor = BayesRegressor(dir_model, name=r'$\mathrm{Dir}$')
 
 # %% Results
 n_test = 100
-n_mc = 50
+n_mc = 1000
 
 # Bayes Squared-Error vs. N
 n_train = np.arange(0, 4050, 50)
-dir_params = {'alpha_0': model.alpha_0 * np.logspace(-1, 1, 3)}
+dir_params = {'alpha_0': [40, 400, 4000]}
 
-# img_path = img_dir + 'risk_bayes_N_leg_a0.png'
+img_path = img_dir + 'risk_bayes_N_leg_a0.png'
 y_stats_full, loss_full = dir_predictor.assess(model, dir_params, n_train, n_test, n_mc,
                                                verbose=True, plot_loss=True, print_loss=False,
                                                log_path=log_path, img_path=img_path, rng=seed)
@@ -54,7 +55,7 @@ y_stats_full, loss_full = dir_predictor.assess(model, dir_params, n_train, n_tes
 n_train = [0, 100, 200, 400, 800]
 dir_params = {'alpha_0': np.sort(np.concatenate((np.logspace(-0., 5., 60), [model.alpha_0])))}
 
-# img_path = img_dir + 'risk_bayes_a0_leg_N.png'
+img_path = img_dir + 'risk_bayes_a0_leg_N.png'
 y_stats_full, loss_full = dir_predictor.assess(model, dir_params, n_train, n_test, n_mc,
                                                verbose=True, plot_loss=True, print_loss=False,
                                                log_path=log_path, img_path=img_path, rng=seed)
