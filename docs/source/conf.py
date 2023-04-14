74929434751181ffa54345fb629aeeef66337c14
Paul Rademacher <paul.rademacher@nrl.navy.mil>
2023-04-14 10:26:21 -0400

    Switch to Furo docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns: list = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -57,10 +57,12 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
+# html_theme = "sphinx_rtd_theme"
 # html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path: list = []
+# html_static_path = ["_static"]
